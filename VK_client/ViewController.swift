86513267816bfa82b94ae1467e9e42d1f4458657
Ekaterina <katrine.d@gmail.com>
2020-07-29 16:58:41 +0300

    Initial Commit

---
+++ VK_client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK_client
+//
+//  Created by Ekaterina Donskaya on 29/07/2020.
+//  Copyright © 2020 Ekaterina Donskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
