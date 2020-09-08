f484edf16f5e4e2c6169f5c0f5429d0a83744338
Ekaterina <katrine.d@gmail.com>
2020-09-08 13:43:49 +0300

    Lesson_8

--- VK_client/News/NewsViewController.swift
+++ VK_client/News/NewsViewController.swift
@@ -63,6 +63,7 @@ class NewsViewController: UIViewController, UITableViewDelegate, UITableViewData
         return UITableView.automaticDimension
     }
     
+ 
     
     // Override to support conditional editing of the table view.
     /* override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
