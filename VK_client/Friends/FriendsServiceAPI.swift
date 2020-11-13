d81157ffa24f9884ab9dc624423977aeb5374f15
Ekaterina Donskaya <katrine.d@gmail.com>
2020-11-13 15:44:25 +0200

    Promise

--- VK_client/Friends/FriendsServiceAPI.swift
+++ VK_client/Friends/FriendsServiceAPI.swift
@@ -12,33 +12,71 @@ import RealmSwift
 import FirebaseDatabase
 import FirebaseFirestore
 import FirebaseFirestoreSwift
+import PromiseKit
 
 class FriendsService {
     
     let baseUrl = "https://api.vk.com"
+    
+//    func getFriend(user_id: String) -> Promise<[Friend]> {
+//
+//        let access_token = Session.instance.token
+//        let path = "/method/friends.get"
+//        let parameters: Parameters = [
+//            Session.instance.userId: user_id,
+//            "order": "hints",
+//            "fields": "photo",
+//            "method": "friends.get",
+//            "access_token": access_token,
+//            "v": "5.124"
+//        ]
+//
+//        let url = baseUrl+path
+//        let promise = Promise<[Friend]> { resolver in
+//            AF.request(url, method: .get, parameters: parameters).responseData { [weak self] response in
+//                switch response.result {
+//                case let .success(data):
+//                    let friend = try! JSONDecoder().decode(FriendResponse.self, from: data).response.items
+//                    resolver.fulfill(friend)
+//                    self?.saveFriends(friend)
+//                    self?.saveFriendsToFirestore(friend)
+//                case let .failure(error):
+//                    resolver.reject(error)
+//                }
+//
+//            }
+//        }
+//        return promise
+//    }
 
-    func getFriend(user_id: String) {
-        let access_token = Session.instance.token
+    func getFriend() -> Promise<[Friend]> {
+
         let path = "/method/friends.get"
         let parameters: Parameters = [
-            Session.instance.userId: user_id,
+            "user_id": Session.instance.userId,
             "order": "hints",
             "fields": "photo",
             "method": "friends.get",
-            "access_token": access_token,
+            "access_token": Session.instance.token,
             "v": "5.124"
         ]
-        
-        let url = baseUrl+path
-        
-        AF.request(url, method: .get, parameters: parameters).responseData { [weak self] response in
-            guard let data = response.value else { return }
-            let friend = try! JSONDecoder().decode(FriendResponse.self, from: data).response.items
 
-            self?.saveFriends(friend)
-            self?.saveFriendsToFirestore(friend)
+        let url = baseUrl+path
+        return Promise<[Friend]> { resolver in
+            AF.request(url, method: .get, parameters: parameters).responseData { [weak self] response in
+                switch response.result {
+                case let .success(data):
+                    let friend = try! JSONDecoder().decode(FriendResponse.self, from: data).response.items
+                    resolver.fulfill(friend)
+                    self?.saveFriends(friend)
+                    self?.saveFriendsToFirestore(friend)
+                case let .failure(error):
+                    resolver.reject(error)
+                }
+
+            }
         }
-        
+       
     }
     
     
