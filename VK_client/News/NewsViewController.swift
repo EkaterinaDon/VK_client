141fceeda107d511b206e03665f49b58537ce98a
Ekaterina Donskaya <katrine.d@gmail.com>
2020-11-23 17:21:28 +0200

    Lesson_7

--- VK_client/News/NewsViewController.swift
+++ VK_client/News/NewsViewController.swift
@@ -8,69 +8,37 @@
 
 import UIKit
 
-
-class NewsModel {
-
-    static let instance = NewsModel()
-
-    var allNews: [News] = []
-
-    var groupNews: [NewsFromGroup] = []
-
-    var profileNews: [Profile] = []
-    
-    var newsPhotos: [String] = []
-
-    func newsOwner() {
-        for item in allNews {
-            if item.sourceId >= 0 {
-                for profile in profileNews {
-                    if item.sourceId == profile.id {
-                        item.newsName = profile.name
-                        item.newsPhoto = profile.photo50!
-                    }
-                }
-            } else {
-                for group in groupNews {
-                    if abs(item.sourceId) == group.id {
-                        item.newsName = group.name
-                        item.newsPhoto = group.photo50!
-                    }
-                }
-            }
-        }
-    }
-    
-    func imagesFromUrls() {
-        newsPhotos = allNews.compactMap { $0.attachments }.reduce([], +).compactMap { $0.photo }.compactMap { $0.sizes }.reduce([], +).compactMap { $0.url }
-        debugPrint("======", newsPhotos)
-    }
-
-}
-
 class NewsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     
     @IBOutlet var table: UITableView!
     
     var newsServise = NewsService()
-
+    var allNews: [News] = []
+    var newsPhotos: [String] = []
+    let refreshControl = UIRefreshControl()
+    var nextFrom = ""
+    var isLoading = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
-            self.newsServise.getNews(user_id: Session.instance.userId) { [weak self] allNews, groupNews, profileNews  in
-                    NewsModel.instance.allNews = allNews
-                    NewsModel.instance.groupNews = groupNews
-                    NewsModel.instance.profileNews = profileNews
-                NewsModel.instance.imagesFromUrls()
-                    self!.table.reloadData()            
+        
+        self.newsServise.getNews(user_id: Session.instance.userId, startFrom: "20.11.2019") { [weak self] allNews, nextFrom  in
+            self?.allNews = allNews
+            self?.nextFrom = nextFrom
+            self!.table.reloadData()
         }
+        
+         newsPhotos = allNews.compactMap { $0.attachments }.reduce([], +).compactMap { $0.photo }.compactMap { $0.sizes }.reduce([], +).compactMap { $0.url }
+        
+        setupRefreshControl()
         
         table.register(NewsTableViewCell.nib(), forCellReuseIdentifier: NewsTableViewCell.identifier)
         
         table.delegate = self
         table.dataSource = self
+        table.prefetchDataSource = self
         
         table.rowHeight = UITableView.automaticDimension
         table.estimatedRowHeight = UITableView.automaticDimension
@@ -81,16 +49,15 @@ class NewsViewController: UIViewController, UITableViewDelegate, UITableViewData
     // MARK: - Table view data source
  
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return NewsModel.instance.allNews.count
+        return allNews.count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: NewsTableViewCell.identifier, for: indexPath) as! NewsTableViewCell
         
-        NewsModel.instance.newsOwner()
-        let new = NewsModel.instance.allNews[indexPath.row]
-        
+        let new = allNews[indexPath.row]
+               
         let date = Date(timeIntervalSince1970: new.date!)
         let stringDate = NewsTableViewCell.dateFormatter.string(from: date)
         cell.dateLabel.text = stringDate
@@ -98,7 +65,7 @@ class NewsViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.newsLabel.text = new.text
         cell.nameLabel.text = new.newsName
         
-        let url = URL(string: new.newsPhoto)
+        let url = URL(string: new.newsPhoto) 
         
         UIImage.loadNewsImage(url: url!) { image in
             cell.photoImage.image = image
@@ -114,6 +81,47 @@ class NewsViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
+    }
+
+    // MARK: - RefreshControl
+    
+    fileprivate func setupRefreshControl() {
+        
+        refreshControl.attributedTitle = NSAttributedString(string: "Refreshing...")
+        refreshControl.tintColor = .black
+        refreshControl.addTarget(self, action: #selector(refreshNews), for: .valueChanged)
+        table.refreshControl = refreshControl
+        
+    }
+    
+    @objc func refreshNews() {
+        
+        self.refreshControl.beginRefreshing()
+        
+        let mostFreshNewsDate = allNews.first?.date ?? Date().timeIntervalSince1970
+        // отправляем сетевой запрос загрузки новостей
+        newsServise.getNews(user_id: Session.instance.userId, startFrom: "21.11.2019", startTime: mostFreshNewsDate + 1) { [weak self] allNews, nextFrom in
+            guard let self = self else { return }
+            self.refreshControl.endRefreshing()
+            // проверяем, что более свежие новости действительно есть
+            guard allNews.count > 0 else { return }
+            // прикрепляем их в начало отображаемого массива
+            self.allNews = allNews + self.allNews
+            self.nextFrom = nextFrom
+            // формируем IndexSet свежедобавленных секций и обновляем таблицу
+            let indexSet = IndexSet(integersIn: 0..<self.allNews.count)
+            //self.table.insertSections(indexSet, with: .automatic)
+            //self.table.insertRows(at: indexSet, with: .automatic)
+            
+        }
+        
+    }
+    
+    private func makeIndexSet(lastIndex: Int, _ newsCount: Int) -> [IndexPath] {
+        let last = lastIndex + newsCount
+        let indexPaths = Array(lastIndex + 1...last).map { IndexPath(row: $0, section: 0) }
+        
+        return indexPaths
     }
     
 }
@@ -135,4 +143,25 @@ extension UIImage {
         }
     }
 
+}
+
+extension NewsViewController: UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        guard !isLoading else { return }
+
+        for indexPath in indexPaths {
+        if indexPath.row == allNews.count - 1 {
+            isLoading = true
+            let lastIndex = indexPath.row
+            newsServise.getNews(user_id: Session.instance.userId, startFrom: nextFrom) { [weak self] allNews, nextFrom  in
+                guard let self = self else { return }
+                self.allNews.append(contentsOf: allNews)
+                self.nextFrom = nextFrom
+                let indexPaths = self.makeIndexSet(lastIndex: lastIndex, allNews.count)
+                self.table.insertRows(at: indexPaths, with: .automatic)
+                self.isLoading = false
+            }
+        }
+    }
+ }
 }
